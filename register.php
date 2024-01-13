b10bcc5b9e155b78360db9fb3251a97fa5017c3a
Donna Moncigil Losantas <92492865+tiny1nefariousness@users.noreply.github.com>
2024-01-14 02:18:29 +0800

    Add files via upload

---
+++ register.php
@@ -0,0 +1,82 @@
+<!--Written by: Donna Moncigil Losantas(BI21110374)-->
+
+<?php
+
+include 'config.php';
+
+if(isset($_POST['submit'])){
+
+   $user_name = mysqli_real_escape_string($conn, $_POST['user_name']);
+   $user_email = mysqli_real_escape_string($conn, $_POST['user_email']);
+   $password = mysqli_real_escape_string($conn, md5($_POST['password']));
+   $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
+   $profile_img = $_FILES['profile_img']['name'];
+   $image_size = $_FILES['profile_img']['size'];
+   $image_tmp_name = $_FILES['profile_img']['tmp_name'];
+   $image_folder = 'uploaded_img/'.$profile_img;
+
+   $select = mysqli_query($conn, "SELECT * FROM `user` WHERE user_email = '$user_email' AND password = '$password'") or die('query failed');
+
+   if(mysqli_num_rows($select) > 0){
+      $message[] = 'user already exist'; 
+   }else{
+      if($password != $cpass){
+         $message[] = 'confirm password not matched!';
+      }elseif($image_size > 2000000){
+         $message[] = 'image size is too large!';
+      }else{
+         $insert = mysqli_query($conn, "INSERT INTO `user`(user_name, user_email, password, profile_img) VALUES('$user_name', '$user_email', '$password', '$profile_img')") or die('query failed');
+
+         if($insert){
+            move_uploaded_file($image_tmp_name, $image_folder);
+            $message[] = 'registered successfully!';
+            header('location:login.php');
+         }else{
+            $message[] = 'registeration failed!';
+         }
+      }
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>register</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<?php include 'menu.php'; ?>
+
+<div class="form-container">
+
+   <form action="" method="post" enctype="multipart/form-data">
+      <h3>register now</h3>
+      <?php
+      if(isset($message)){
+         foreach($message as $message){
+            echo '<div class="message">'.$message.'</div>';
+         }
+      }
+      ?>
+      <input type="text" name="user_name" placeholder="enter username" class="box" required>
+      <input type="email" name="user_email" placeholder="enter email" class="box" required>
+      <input type="password" name="password" placeholder="enter password" class="box" required>
+      <input type="password" name="cpassword" placeholder="confirm password" class="box" required>
+      <input type="file" name="profile_img" class="box" accept="image/jpg, image/jpeg, image/png">
+      <input type="submit" name="submit" value="submit" class="btn">
+      <p>already have an account? <a href="login.php">login now</a></p>
+   </form>
+
+</div>
+
+</body>
+</html>
---
+++ update_profile.php
@@ -0,0 +1,115 @@
+<!--Written by: Donna Moncigil Losantas(BI21110374)-->
+
+<?php
+
+include 'config.php';
+session_start();
+$user_id = $_SESSION['user_id'];
+
+if(isset($_POST['update_profile'])){
+
+   $update_name = mysqli_real_escape_string($conn, $_POST['update_name']);
+   $update_email = mysqli_real_escape_string($conn, $_POST['update_email']);
+
+   mysqli_query($conn, "UPDATE `user` SET user_name = '$update_name', user_email = '$update_email' WHERE user_id = '$user_id'") or die('query failed');
+
+   $old_pass = $_POST['old_pass'];
+   $update_pass = mysqli_real_escape_string($conn, md5($_POST['update_pass']));
+   $new_pass = mysqli_real_escape_string($conn, md5($_POST['new_pass']));
+   $confirm_pass = mysqli_real_escape_string($conn, md5($_POST['confirm_pass']));
+
+   if(!empty($update_pass) || !empty($new_pass) || !empty($confirm_pass)){
+      if($update_pass != $old_pass){
+         $message[] = 'old password not matched!';
+      }elseif($new_pass != $confirm_pass){
+         $message[] = 'confirm password not matched!';
+      }else{
+         mysqli_query($conn, "UPDATE `user` SET password = '$confirm_pass' WHERE user_id = '$user_id'") or die('query failed');
+         $message[] = 'password updated successfully!';
+      }
+   }
+
+   $update_image = $_FILES['update_image']['name'];
+   $update_image_size = $_FILES['update_image']['size'];
+   $update_image_tmp_name = $_FILES['update_image']['tmp_name'];
+   $update_image_folder = 'uploaded_img/'.$update_image;
+
+   if(!empty($update_image)){
+      if($update_image_size > 2000000){
+         $message[] = 'image is too large';
+      }else{
+         $image_update_query = mysqli_query($conn, "UPDATE `user` SET profile_img = '$update_image' WHERE user_id = '$user_id'") or die('query failed');
+         if($image_update_query){
+            move_uploaded_file($update_image_tmp_name, $update_image_folder);
+         }
+         $message[] = 'image updated succssfully!';
+      }
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width,  initial-scale=1.0">
+   <title>Update Profile</title>
+   <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,400&family=Raleway:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<?php include 'user_menu.php'; ?>
+
+<div class="update-profile">
+
+   <?php
+      $select = mysqli_query($conn, "SELECT * FROM `user` WHERE user_id = '$user_id'") or die('query failed');
+      if(mysqli_num_rows($select) > 0){
+         $fetch = mysqli_fetch_assoc($select);
+      }
+   ?>
+
+   <form action="" method="post" enctype="multipart/form-data">
+      <?php
+         if($fetch['profile_img'] == ''){
+            echo '<img src="images/default-avatar.png">';
+         }else{
+            echo '<img src="uploaded_img/'.$fetch['profile_img'].'">';
+         }
+         if(isset($message)){
+            foreach($message as $message){
+               echo '<div class="message">'.$message.'</div>';
+            }
+         }
+      ?>
+      <div class="flex">
+         <div class="inputBox">
+            <span>username :</span>
+            <input type="text" name="update_name" value="<?php echo $fetch['user_name']; ?>" class="box">
+            <span>your email :</span>
+            <input type="email" name="update_email" value="<?php echo $fetch['user_email']; ?>" class="box">
+            <span>update your pic :</span>
+            <input type="file" name="update_image" accept="image/jpg, image/jpeg, image/png" class="box">
+         </div>
+         <div class="inputBox">
+            <input type="hidden" name="old_pass" value="<?php echo $fetch['password']; ?>">
+            <span>old password :</span>
+            <input type="password" name="update_pass" placeholder="enter previous password" class="box">
+            <span>new password :</span>
+            <input type="password" name="new_pass" placeholder="enter new password" class="box">
+            <span>confirm password :</span>
+            <input type="password" name="confirm_pass" placeholder="confirm new password" class="box">
+         </div>
+      </div>
+      <input type="submit" value="update profile" name="update_profile" class="btn">
+      <a href="home.php" class="delete-btn">go back</a>
+   </form>
+
+</div>
+
+</body>
+</html>
