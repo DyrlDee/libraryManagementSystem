add52a2d5ea8189834022e9b0b4875f8e4fcd4d9
xenxce <87857323+xenxce@users.noreply.github.com>
2024-01-14 23:21:00 +0800

    Update book_report.php

--- book_report.php
+++ book_report.php
@@ -1,32 +1,25 @@
-<!-- 
-
-    DISCLAIMER
-
-    This is not the complete version of the report; configurations will be made after login, 
-    and the book loan module is available. Linking between different PHP files is required 
-    for it to be automatically displayed in the report. A dummy database is set up solely 
-    for testing the functionality of the code.
-
-    Configuration will be done during the compilation of codes.
-
- -->
-
 <?php
+session_start();
 include("config.php");
 
-// Fetch notifications for the current user
-if (isset($_SESSION["user_id"])) {
-    $user_id = $_SESSION["user_id"];
-    $query = "SELECT * FROM notification WHERE user_id = $user_id";
-    $result = mysqli_query($conn, $query);
+$book_id;
 
-    if (!$result) {
-        die("Query failed: " . mysqli_error($conn));
-    }
+if(isset($_GET["book_id"]) && $_GET["book_id"] != ""){
 
-    // Fetched notifications array
-    $notifications = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    $book_id = $_GET["book_id"];
+
 }
+
+$query = "SELECT * FROM book WHERE book_id = $book_id";
+$result = mysqli_query($conn, $query);
+
+if (!$result) {
+    die("Query failed: " . mysqli_error($conn));
+}
+
+// Fetch the data from the result set
+$row = mysqli_fetch_assoc($result);
+
 ?>
 
 <!DOCTYPE html>
@@ -35,7 +28,7 @@ if (isset($_SESSION["user_id"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Report</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,400&family=Raleway:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
@@ -57,86 +50,36 @@ if (isset($_SESSION["user_id"])) {
             include 'user_menu.php';
         ?>
 
-        
-
     <!-- report bar -->
 
     <div class="container">
         <h1 class = "title">Report Book</h1>
-            <div class = "book_grid">
-                <?php
-                    $user_id = $_SESSION["user_id"];
-                    $sql = "SELECT * FROM book WHERE user_id = '$user_id' AND book_name = '$book_name'";
-                ?>
+        <div class = "book_grid">
+
+            <form action="book_action.php" method="POST" style="width: 60%;">
+                <div class="photo_grid">
+                <img src="img/<?php echo $row['book_img']; ?>" alt="Book Image">
+                </div>
+
+                <div> <b>Book Name:</b> <?php echo $row["book_name"]; ?> </div>
+               
+                <div > <b>Author:</b>  <?php echo isset($row["book_author"]) ? $row["book_author"] : ""; ?></div>
                 
-                <form action="book_action.php" method="POST">
-                    <div class="photo_grid">
-                        <!-- <img src="img/ToKillAMockingbird.jpg" alt="Book Image"> -->
-                        <img src="img/<?php echo $book_img; ?>" alt="Book Image">
-                    </div>
+                <div > <b>Publisher:</b>  <?php echo isset($row["book_publisher"]) ? $row["book_publisher"] : ""; ?></div>
+               
+                <div > <b>Genre:</b>  <?php echo isset($row["book_genre"]) ? $row["book_genre"] : ""; ?></div>
 
-                    <div class = "table_grid">
-                        <div class="section_table_2">
-                            <table class = "facility_report">
-                                <tr>
-                                    <td class = "faci_title"> Book Name: </td>
-                                    <?php
-                                        if ($result) {
-                                            $row = mysqli_fetch_assoc($result);
-                                            if ($row) {
-                                                echo "<td>" . (isset($row["book_name"]) ? $row["book_name"] : "") . "</td>";
-                                            } else {
-                                                echo "No data found";
-                                            }
-                                        }
-                                    ?>
-                                </tr>
-                                <tr>
-                                    <td class = "faci_title"> Author: </td>
-                                    <?php
-                                        if ($result) {
-                                            $row = mysqli_fetch_assoc($result);
-                                            if ($row) {
-                                                echo "<td>" . (isset($row["book_author"]) ? $row["book_author"] : "") . "</td>";
-                                            } else {
-                                                echo "No data found";
-                                            }
-                                        }
-                                    ?>
-                                </tr>
-                                <tr>
-                                    <td class = "faci_title"> Publisher: </td>
-                                    <?php
-                                        if ($result) {
-                                            $row = mysqli_fetch_assoc($result);
-                                            if ($row) {
-                                                echo "<td>" . (isset($row["book_publisher"]) ? $row["book_publisher"] : "") . "</td>";
-                                            } else {
-                                                echo "No data found";
-                                            }
-                                        }
-                                    ?>
-                                </tr>
-                                <tr>
-                                    <td class = "faci_title"> Report Category: </td>
-                                    <td>Book</td>
-                                </tr>
-                                <tr>
-                                    <td class = "faci_title_d"> Report Description: </td>
-                                </tr>
-                                <tr>
-                                    <td><textarea rows="4" name="report_description" cols="20"></textarea></td>
-                                </tr>
-                            </table> 
-                        </div>
-                    </div>
+                <input type="hidden" name="book_id" value="<?php echo $book_id;?>">
+                <label for="">Report Description:</label><br>
+                <textarea rows="4" name="report_description" cols="40"></textarea>
 
 
-            </div>
-            <div class="search">
+                </div>
+
+                <div class="search">
                 <input type="submit" name="" id="" value="submit">
-            </div>
-        </form>
+                </div>
+            </form>
     </div>
 
     <footer>
