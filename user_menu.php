e78d57857ce5280723e154080935b6bd4a3ff481
xenxce <87857323+xenxce@users.noreply.github.com>
2024-01-14 23:30:40 +0800

    Update user_menu.php

--- user_menu.php
+++ user_menu.php
@@ -8,7 +8,7 @@ echo '<header id="myTopHeader">
     <a href="book_module_page.php" class="not_icon">Book</a>
     <a href="facility_module.php" class="not_icon">Facility</a>
     <a href="user_fine_page.php" class="not_icon">Fine</a>
-    <a href="#" class="not_icon">Report</a>
+    <a href="view_report.php" class="not_icon">Report</a>
     <a href="notification.php"><i class="fa fa-bell" aria-hidden="true"></i></a>
     <a href="logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i></a>
     <a href="javascript:void(0);" class="icon" onclick="myFunction()">
