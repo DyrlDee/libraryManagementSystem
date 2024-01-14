2ba3d86ebd0a806ca70ddbb6f3a9f7c9fa598bf1
xenxce <87857323+xenxce@users.noreply.github.com>
2024-01-14 23:19:34 +0800

    Update facility_report.php

--- facility_report.php
+++ facility_report.php
@@ -1,32 +1,27 @@
-<!-- 
-
-    DISCLAIMER
-
-    This is not the complete version of the report; configurations will be made after login, 
-    and the book loan module is available. Linking between different PHP files is required 
-    for it to be automatically displayed in the report. A dummy database is set up solely 
-    for testing the functionality of the code.
-
-    Configuration will be done during the compilation of codes.
-
- -->
-
 <?php
+session_start();
 include("config.php");
 
-// Fetch notifications for the current user
 if (isset($_SESSION["user_id"])) {
     $user_id = $_SESSION["user_id"];
-    $query = "SELECT * FROM notification WHERE user_id = $user_id";
-    $result = mysqli_query($conn, $query);
-
-    if (!$result) {
-        die("Query failed: " . mysqli_error($conn));
-    }
-
-    // Fetched notifications array
-    $notifications = mysqli_fetch_all($result, MYSQLI_ASSOC);
 }
+
+if(isset($_GET["faci_id"]) && $_GET["faci_id"] != ""){
+
+    $faci_id = $_GET["faci_id"];
+}
+
+
+$query = "SELECT * FROM facility WHERE faci_id = $faci_id ";
+$result = mysqli_query($conn, $query);
+
+if (!$result) {
+    die("Query failed: " . mysqli_error($conn));
+}
+
+// Fetch the data from the result set
+$row = mysqli_fetch_assoc($result);
+
 ?>
 
 <!DOCTYPE html>
@@ -35,7 +30,7 @@ if (isset($_SESSION["user_id"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Report</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,400&family=Raleway:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
@@ -61,7 +56,6 @@ if (isset($_SESSION["user_id"])) {
     <div class="container">
     <?php
         $user_id = $_SESSION["user_id"];
-        $sql = "SELECT * FROM facility WHERE user_id = '$user_id' AND faci_name = '$faci_name'";
     ?>
         <h1 class = "title">Report Facility</h1>
         <form action="faci_action.php" method="POST">
@@ -70,40 +64,23 @@ if (isset($_SESSION["user_id"])) {
                 <table class = "facility_report">
                     <tr>
                         <td class = "faci_title"> Room Name: </td>
-                        <?php
-                            if ($result) {
-                                $row = mysqli_fetch_assoc($result);
-                                if ($row) {
-                                    echo "<td>" . (isset($row["faci_name"]) ? $row["faci_name"] : "") . "</td>";
-                                } else {
-                                    echo "No data found";
-                                }
-                            }
-                        ?>
+                        <td><?php echo isset($row["faci_name"]) ? $row["faci_name"] : ""; ?></td>
                     </tr>
                     <tr>
                         <td class = "faci_title"> Faci Type: </td>
-                        <?php
-                            if ($result) {
-                                $row = mysqli_fetch_assoc($result);
-                                if ($row) {
-                                    echo "<td>" . (isset($row["faci_type"]) ? $row["faci_type"] : "") . "</td>";
-                                } else {
-                                    echo "No data found";
-                                }
-                            }
-                        ?>
+                        <td><?php echo isset($row["faci_type"]) ? $row["faci_type"] : ""; ?></td>
                     </tr>
                     <tr>
                         <td class = "faci_title"> Report Category: </td>
                         <td>Facility</td>
                     </tr>
+                    
                     <tr>
-                        <td class = "faci_title_d"> Report Description: </td>
-                    </tr>
-                    <tr>
+                        <td class = "faci_title_d" style="text-align:center; "> Report Description: </td>
                         <td><textarea rows="4" name="report_description" cols="20"></textarea></td>
                     </tr>
+
+                    <input type="hidden" name="faci_id" value="<?php echo $faci_id;?>">
                 </table> 
             </div>
             
