339ed980785905ea02ff8224a6f130269ae985af
siti shahira <dsitishahira14@gmail.com>
2025-01-20 04:00:13 +0800

    database

--- config.php
+++ config.php
@@ -3,8 +3,12 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "librarylms";
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-if (!$conn) {
- die("Connection failed: " . mysqli_connect_error());
+
+// Create connection with XAMPP MySQL socket path
+$conn = new mysqli($servername, $username, $password, $dbname, null, '/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock');
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
